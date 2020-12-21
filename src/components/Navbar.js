87ba3d15576281178631330789878501513513f4
Josh Seckman <jseckman90@gmail.com>
2020-12-20 21:47:12 -0500

    nav set up with Links and some styling added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 const Navbar = (props) => {
   return (
-    <div>
-      <h3>Hello from Navbar</h3>
-    </div>
+    <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <Link to="/">LOGO</Link>
+
+      <ul className="navbar-nav algin-items-center">
+        <li className=" nav-item ml-5">
+          <Link to="/" className="nav-link">
+            products
+          </Link>
+        </li>
+      </ul>
+      <Link to="/cart" className="ml-auto">
+        <ButtonContainer>
+          <span className="mr-2">
+            <i className="fas fa-cart-plus" />
+          </span>
+          My Cart
+        </ButtonContainer>
+      </Link>
+    </NavWrapper>
   );
 };
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
 
 export default Navbar;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Product from "./Product";
+import Title from "./Title";
 
 const ProductList = (props) => {
   return (
-    <div>
-      <Product />
-    </div>
+    <>
+      <div className="py-5">
+        <div className="container">
+          <Title name="our" title="products" />
+          <div className="row"></div>
+        </div>
+      </div>
+    </>
+    // <Product />
   );
 };
 
